f540248ff01dfd71af4accd46934842673690848
BossDodo <30752013+NortexYT@users.noreply.github.com>
2024-09-19 12:33:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000); // Update the time every second
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  return (
+    <div>
+      Volam sa Dodik  {currentTime}.
+    </div>
+  );
 }
 
 export default App;
